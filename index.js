311e3a9a19811bb7716e7750b80cf8c2e176e38e
RWOverdijk <r.w.overdijk@gmail.com>
2017-10-10 12:07:25 +0200

    chore(project): export new generators

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const {Boards}     = require('./lib/Boards');
-const {Generator}  = require('./lib/Generator');
+const {Boards}                = require('./lib/Boards');
+const {Generator}             = require('./lib/Generator');
+const {ModificationGenerator} = require('./lib/generator/ModificationGenerator');
+const {TemplateGenerator}     = require('./lib/generator/TemplateGenerator');
 
-module.exports = {Boards, Generator};
+module.exports = {Boards, Generator, ModificationGenerator, TemplateGenerator};
